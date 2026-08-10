3eda75ced710bb5e36983e9384dca81f524b2270
sivajayaraman <kit28.24cs162@gmail.com>
2026-08-10 22:16:07 +0530

    change hello.cpp and readme.md

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,24 @@
 #include<iostream>
 using namespace std;
+class hello
+{
+private:
+    /* data */
+public:
+    hello(/* args */);
+    ~hello();
+};
+
+hello::hello(/* args */)
+{
+}
+
+hello::~hello()
+{
+}
+
 int main(){
+    hello h;
     cout<<"hello siva";
     cout<<"siva aruna ganga";
     cout<<"i love yoy";
--- readme.md
+++ readme.md
@@ -1,2 +1,4 @@
 ## this is siva
 --heyy frinds how are you 
+-- i love my sister and my brother 
+-- heyy how are yoy everyone
